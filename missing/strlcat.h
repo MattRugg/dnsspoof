6dfad37462d4a3737a3ec7d970a09cee2fb2ad65
Matheus <matheusr@rfdesign.com.au>
2020-11-29 21:50:58 +1000

    Only compiles dnsspoof now
    Patched to meet current libnet function prototypes.

---
+++ missing/strlcat.h
@@ -0,0 +1,6 @@
+#ifndef STRLCAT_H
+#define STRLCAT_H
+
+size_t strlcat(char *dst, const char *src, size_t siz);
+
+#endif /* STRLCAT_H */
---
+++ missing/strlcpy.h
@@ -0,0 +1,6 @@
+#ifndef STRLCPY_H
+#define STRLCPY_H
+
+size_t strlcpy(char *dst, const char *src, size_t siz);
+
+#endif /* STRLCPY_H */
